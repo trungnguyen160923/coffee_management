7fbeb922353c684b2c12c38ba6226399c1f1cf8e
trungnguyen160923 <nguyenthanht632@gmail.com>
2025-09-27 11:45:53 +0700

    config order service

---
+++ order-service/src/main/java/orderservice/order_service/exception/AppException.java
@@ -0,0 +1,29 @@
+package orderservice.order_service.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, String customMessage) {
+        super(customMessage);
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+
+    private ErrorCode errorCode;
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
