39e6ef12a7ff748b16eefc5e7566a68cd43a2147
trungnguyen160923 <nguyenthanht632@gmail.com>
2025-11-29 13:57:46 +0700

    deploy part 2: Add base path /admin for frontend-admin, fix kafka

--- fe_coffee_manager/vite.config.js
+++ fe_coffee_manager/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/admin/',  // Base path cho production - assets sẽ có path /admin/assets/...
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
